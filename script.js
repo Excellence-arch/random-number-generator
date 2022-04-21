b0da94e64d313bec865003514433e7d90f4d18d0
excellence-arch <oladipupomichael9@gmail.com>
2022-04-21 12:19:06 +0100

    completed the project

---
+++ script.js
@@ -0,0 +1,45 @@
+const random = () => {
+  if (Number(maxNum.value) <= Number(minNum.value)) {
+    maxNum.value = minNum.value + "0";
+  }
+  return (
+    Math.round(Math.random() * (maxNum.value - minNum.value)) +
+    Number(minNum.value)
+  );
+};
+
+let lastMin = "";
+let time = 0;
+
+const genNum = () => {
+  let num = random();
+  lastMin = minNum.value;
+  genNumber.innerHTML = num;
+  // minNum.value = num;
+  time = 0;
+  // clearTime();
+}
+
+let count;
+let timer;
+const randomNumber = () => {
+  time += 1;
+  console.log(time)
+  let num = random();
+  genNumber.innerHTML = num;
+  if(time == 20) {
+    clearInterval(count);
+    genNum();
+  }
+};
+
+
+const generate = () => {
+  count = setInterval(randomNumber, 100);
+}
+
+const callTimer = () => {
+  time = 0;
+  clearInterval(count);
+  generate();
+}
